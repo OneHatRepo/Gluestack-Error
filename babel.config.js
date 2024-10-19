ddacd6b54a7134e5dfb6aebc611508b97fb5f287
Scott Spuler <scott@onehat.com>
2024-10-19 15:54:35 -0700

    fix babel

--- babel.config.js
+++ babel.config.js
@@ -5,11 +5,11 @@ module.exports = function(api) {
             jsxImportSource: "nativewind"
         }], "nativewind/babel"],
 		plugins: [
-            'react-native-classname-to-style',
-            ['react-native-platform-specific-extensions', { extensions: ['css', 'pcss'] }],
+            // 'react-native-classname-to-style',
+            // ['react-native-platform-specific-extensions', { extensions: ['css', 'pcss'] }],
             // '@babel/plugin-transform-class-properties',
-            '@babel/plugin-transform-export-namespace-from',
-            '@babel/plugin-transform-runtime',
+            // '@babel/plugin-transform-export-namespace-from',
+            // '@babel/plugin-transform-runtime',
             // require.resolve('expo-router/babel'),
             'react-native-reanimated/plugin',
             ["module-resolver", {
